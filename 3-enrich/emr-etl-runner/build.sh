32620cc3632dcf54b9159ddda8d33eafb317a83f
Peter Greskovic <peter@haensel-ams.com>
2017-05-22 15:30:57 +0200

    changed build for emr runner for docker

--- 3-enrich/emr-etl-runner/build.sh
+++ 3-enrich/emr-etl-runner/build.sh
@@ -15,10 +15,12 @@
 # Copyright:: Copyright (c) 2015 Snowplow Analytics Ltd
 # License::   Apache License Version 2.0
 
-source ~/.rvm/scripts/rvm
+# MODIFIED by HaenselAMS (2017-05-22)
 
-rvm install jruby-9.1.6.0
-rvm use --default jruby-9.1.6.0
+source /usr/local/rvm/scripts/rvm
+
+rvm install jruby-9.1.7.0
+rvm use --default jruby-9.1.7.0
 gem install bundler
 bundle install
 echo 'Running RSpec'
